5c8a25c836d8d24556224da17b5544525021eb43
Ruslan Mukhametshin <rusmux73@gmail.com>
2025-01-04 13:58:45 +0300

    feat(MongoDB): dynamically get shard replicas

--- 2-sharding/scripts/mongodb-init.sh
+++ 2-sharding/scripts/mongodb-init.sh
@@ -2,38 +2,39 @@
 
 set -eux
 
+get_shard_members() {
+  shard_number="$1"
+  docker ps --filter name=mongodb-shard-"$shard_number" --format "{{.Names}}" | \
+    sort -t '-' -k 3n | \
+    awk '{ printf "{_id: %d, host: \\\"%s:27018\\\"},", NR-1, $1 }'
+}
+
 sleep 5  # wait for the containers to start
 
-docker exec mongodb-configSrv sh -c 'echo "
+docker exec mongodb-configSrv sh -c "echo \"
 rs.initiate({
-    _id: \"config-server\",
+    _id: 'config-server',
     configsvr: true,
-    members: [{_id: 0, host: \"mongodb-configSrv:27019\"}]
+    members: [{_id: 0, host: 'mongodb-configSrv:27019'}]
 });
-" | mongosh --port 27019'
+\" | mongosh --port 27019"
 
-docker exec mongodb-shard-1-1 sh -c 'echo "
-rs.initiate({
-    _id: \"shard-1\",
-    members: [{_id: 0, host: \"mongodb-shard-1-1:27018\"}]
-});
-" | mongosh --port 27018'
+docker exec mongodb-shard-1-1 sh -c "echo \"
+rs.initiate({_id: 'shard-1', members: [$(get_shard_members 1)]});
+\" | mongosh --port 27018"
 
-docker exec mongodb-shard-2-1 sh -c 'echo "
-rs.initiate({
-    _id: \"shard-2\",
-    members: [{_id: 0, host: \"mongodb-shard-2-1:27018\"}]
-});
-" | mongosh --port 27018'
+docker exec mongodb-shard-2-1 sh -c "echo \"
+rs.initiate({_id: 'shard-2', members: [$(get_shard_members 2)]});
+\" | mongosh --port 27018"
 
 sleep 5  # wait for the config server to be ready
 
-docker exec mongos-router sh -c 'echo "
-sh.addShard(\"shard-1/mongodb-shard-1-1:27018\");
-sh.addShard(\"shard-2/mongodb-shard-2-1:27018\");
-sh.enableSharding(\"somedb\");
-sh.shardCollection(\"somedb.users\", {\"name\": \"hashed\"});
+docker exec mongos-router sh -c "echo \"
+sh.addShard('shard-1/mongodb-shard-1-1:27018');
+sh.addShard('shard-2/mongodb-shard-2-1:27018');
+sh.enableSharding('somedb');
+sh.shardCollection('somedb.users', {'name': 'hashed'});
 
 use somedb;
-for (var i = 0; i < 1000; i++) db.users.insertOne({age: i, name: \"User \" + i});
-" | mongosh'
+for (var i = 0; i < 1000; i++) db.users.insertOne({age: i, name: 'User ' + i});
+\" | mongosh"
